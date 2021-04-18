01d69ed4384777edfbe55ca3af9497836ebed1cd
V1lko <vsabol9@gmail.com>
2021-04-18 11:29:19 +0200

    Gun Rotate +

---
+++ Assets/GunRotate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunRotate : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 gunPos = Camera.main.WorldToScreenPoint(transform.position);
+        mousePos.x = mousePos.x - gunPos.x;
+        mousePos.y = mousePos.y - gunPos.y;
+        float gunAngle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        if(Camera.main.ScreenToWorldPoint(Input.mousePosition).x < transform.position.x)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(180f, 0f, -gunAngle));
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, gunAngle));
+        }
+    }
+}
