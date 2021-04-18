e7a6bedce95e5b8b45cfa1b0a74f9742a701e624
V1lko <vsabol9@gmail.com>
2021-04-18 11:29:03 +0200

    Bullet +

---
+++ Assets/Bullets.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour
+{
+    //VARIABLES
+    [SerializeField] float speed = 20f;
+    //REFERENCES
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+}   
